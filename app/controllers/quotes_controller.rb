682c8552af60c46908c5b78f0df51108975da1c9
spiraldown <47649528+spiraldown@users.noreply.github.com>
2020-04-09 08:54:08 +0000

    added modal for form

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,10 +3,6 @@ class QuotesController < ApplicationController
     @quote = Quote.order("RANDOM()").first
   end
 
-  def new
-    @quote = Quote.new
-  end
-
   def create  
     @quote = Quote.create(quote_params)
     if @quote.invalid?
@@ -15,8 +11,12 @@ class QuotesController < ApplicationController
     redirect_to root_path
   end
 
+  def about
+  end
+
+  private
+
   def quote_params
     params.require(:quote).permit(:saying, :character, :where, :when, :title)
   end
-
 end
