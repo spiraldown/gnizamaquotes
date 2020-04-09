a4320f5036368d1eff3ab5f35b8758a003db4698
spiraldown <47649528+spiraldown@users.noreply.github.com>
2020-04-09 08:30:55 +0000

    added requirements for submission fields

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,5 @@
 class Quote < ApplicationRecord
+  validates :saying, presence: true, length: { maximum: 140, minimum: 2 }
+  validates :character, presence: true, length: { maximum: 50, minimum: 2}
+  validates :where, presence: true, length: { maximum: 50, minimum: 2}
 end
